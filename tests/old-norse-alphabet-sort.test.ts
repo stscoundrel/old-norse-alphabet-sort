a00e3fb9e917ee03c234db9b10c295740e14e7ba
stscoundrel <sampo@pixels.fi>
2020-12-26 18:24:19 +0200

    Refactor to typescript

---
+++ tests/old-norse-alphabet-sort.test.ts
@@ -0,0 +1,25 @@
+import { oldNorseSort } from '../src';
+
+describe('Old Norse Alphabet sort', () => {
+  test('Throws error on missing arguments', () => {
+    expect(() => {
+      oldNorseSort();
+    }).toThrow('Missing argument: expects two strings as arguments');
+  });
+
+  test('Sorts by Old Norse alphabet', () => {
+    const words = ['öðli', 'ógnan', 'æðrask', 'aðili', 'þakkan', 'áfir', 'á-auki', 'él-ligr', 'maðka', 'ef-lauss', 'œgir', 'áðr', 'maðr', 'madr', 'mæðr'];
+    const expected = ['aðili', 'á-auki', 'áðr', 'áfir', 'ef-lauss', 'él-ligr', 'madr', 'maðka', 'maðr', 'mæðr', 'ógnan', 'þakkan', 'æðrask', 'œgir', 'öðli'];
+    const result = [...words].sort((a, b) => oldNorseSort(a, b));
+
+    expect(result).toEqual(expected);
+  });
+
+  test('Normal way sucks', () => {
+    const words = ['öðli', 'ógnan', 'æðrask', 'aðili', 'þakkan', 'áfir', 'á-auki', 'él-ligr', 'maðka', 'ef-lauss', 'œgir', 'áðr', 'maðr', 'madr', 'mæðr'];
+    const expected = ['aðili', 'á-auki', 'áðr', 'áfir', 'ef-lauss', 'él-ligr', 'madr', 'maðka', 'maðr', 'mæðr', 'ógnan', 'þakkan', 'æðrask', 'œgir', 'öðli'];
+    const result = [...words].sort((a, b) => a.localeCompare(b, 'is'));
+
+    expect(result).toEqual(expected);
+  });
+});
